64efd229ba82b2a36b4b839b2982438eb74c3a75
saitsk35 <saitsk35@gmail.com>
2026-01-27 12:52:41 +0530

    variables

--- common.sh
+++ common.sh
@@ -2,3 +2,9 @@ sample1 () {
   echo Sample1 Function
 }
 
+sample2 () {
+  b=20
+  echo Sample2 Function
+  echo a - $a
+}
+
--- functions.sh
+++ functions.sh
@@ -10,3 +10,8 @@ sample
 # Source common.sh script. meaning load the function and variables inside that script to here
 source common.sh
 sample1
+
+# access variable across functions
+a=10
+source common.sh
+echo b - $b
