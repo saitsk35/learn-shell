e5d3e72407ec21a50857353b68b47cccc145cc43
saitsk35 <saitsk35@gmail.com>
2026-01-27 13:00:39 +0530

    variables

--- functions.sh
+++ functions.sh
@@ -4,14 +4,3 @@ sample() {
   echo Sample Function
 }
 
-# Calling the Function
-sample
-
-# Source common.sh script. meaning load the function and variables inside that script to here
-source common.sh
-sample1
-
-# access variable across functions
-a=10
-sample2
-echo b - $b
