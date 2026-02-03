55d494c4b959605eace2f60e468828dbd2578079
saitsk35 <saitsk35@gmail.com>
2026-02-03 16:19:25 +0530

    variables

--- for-loop.sh
+++ for-loop.sh
@@ -1,18 +1,18 @@
 for FRUIT in apple orange banana; do
   echo Fruit Name - $FRUIT
-  sleep 1
+  sleep 0
 done
 
 x=5
 while [ $x -gt 0 ]; do
   echo "Value: $x"
   x=$((x-1))
-  sleep 2
+  sleep 0
 done
 
 y=5
 while [ $y -gt 0 ]; do
   echo "Value: $y"
   y=$((y+1))
-  sleep 2
+  sleep 0
 done
