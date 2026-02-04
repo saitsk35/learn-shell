5fec8db0803b6bd1bd0f08ebc11ae26901ed7758
saitsk35 <saitsk35@gmail.com>
2026-02-04 22:56:42 +0530

    variables

--- functions.sh
+++ functions.sh
@@ -22,13 +22,20 @@ sample() {
   echo x - $x
 }
 
-x=100
+sample1() {
+  echo First argument - $1
+  echo Second argument - $2
+  echo All argument - $*
+  echo No of arguments - $#
+
+}
+
+x=10
 sample
 
-x=200
+x=20
 sample
+
 
 x=100 sample
 x=200 sample
-
-sample1 1000 2000
