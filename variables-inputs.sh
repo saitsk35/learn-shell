3ebd663fc6412569d0dedef82e32adbff14bfa1e
saitsk35 <saitsk35@gmail.com>
2026-02-04 22:18:38 +0530

    variables

--- variables-inputs.sh
+++ variables-inputs.sh
@@ -1,2 +1,14 @@
-x=100
 echo x - $x
+
+
+# Position
+echo First Argument - $1
+echo Second Argument - $2
+echo All Arguments - $*
+echo No of Arguments - $#
+
+
+# read
+
+read -p 'Enter y: ' y
+echo y - $y
