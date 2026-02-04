ebe378f61440bee28f704773258d593cf5261f23
saitsk35 <saitsk35@gmail.com>
2026-02-04 22:52:35 +0530

    variables

--- functions.sh
+++ functions.sh
@@ -1,17 +1,32 @@
-# Declaring the function
+## Declaring the function
+#
+#sample() {
+#  echo Sample Function
+#}
+#
+## Calling the Function
+#sample
+#
+## Source common.sh script. meaning load the function and variables inside that script to here
+#source common.sh
+#sample1
+#
+## access variable across functions
+#a=10
+#sample2
+#echo b - $b
+
+
+
+
+
 
 sample() {
-  echo Sample Function
+  echo x = $x
 }
 
-# Calling the Function
+x=10
 sample
 
-# Source common.sh script. meaning load the function and variables inside that script to here
-source common.sh
-sample1
-
-# access variable across functions
-a=10
-sample2
-echo b - $b
+x=20
+sample
